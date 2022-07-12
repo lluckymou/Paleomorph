e9cf89a954a1b837c1b7722188c5e4800f8fea69
lluckymou <llucmou@gmail.com>
2022-07-12 15:37:26 -0300

    Moving floor/walls

---
+++ Assets/Scripts/Course/WallScroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScroll : MonoBehaviour
+{
+    void Update()
+    {
+        if(transform.position.z <= -104)
+            transform.position = new Vector3(transform.position.x, transform.position.y, 216);
+        
+        transform.Translate(0, 0, -Player.speed * Time.deltaTime, Space.World);
+    }
+}
