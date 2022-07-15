1acad60b7458a05ac45f6dc2949bc18bac1f8572
lluckymou <llucmou@gmail.com>
2022-07-15 19:54:36 -0300

    Banner Textures

--- Assets/Scripts/Menu/Catalog.cs
+++ Assets/Scripts/Menu/Catalog.cs
@@ -17,6 +17,28 @@ public class Catalog : MonoBehaviour
     [SerializeField] Color selected;
     [SerializeField] Color unlocked;
 
+    [Header("Gameobjects")]
+    [SerializeField] GameObject buyButton;
+    [SerializeField] GameObject unlockButton;
+    [SerializeField] GameObject selectButton;
+    [SerializeField] GameObject panel;
+    [SerializeField] Image location;
+    [SerializeField] Image species;
+    [SerializeField] Text speciesName;
+    [SerializeField] Text scientificName;
+    [SerializeField] Text popularName;
+    [SerializeField] Text description;
+
+    [Header("Other")]
+    [SerializeField] Sprite question;
+
+    const string noDescription = @"Características:
+
+  • ???????????? ???????? ?????;
+  • ?????????? ????????? ??????? ????;
+  • ????????????? ???? ????? ?? ??????;
+  • ??????? ???? ?????? ???? ?????;";
+
     void Start()
     {
         if(Inventory.Abelisaurus != UnlockSteps.Locked)
@@ -55,8 +77,113 @@ public class Catalog : MonoBehaviour
         }
     }
 
-    void Update()
+    public void GenerateAbelisaurus()
     {
-        
+        if(Inventory.Abelisaurus == UnlockSteps.Locked) return;
+
+        CatalogInfo ci = GetComponent<CatalogInfo>();
+
+        location.sprite = ci.AbelisaurusLocation;
+        description.text = ci.AbelisaurusText;
+        species.sprite = ci.AbelisaurusSprite;
+        speciesName.text = ci.AbelisaurusName;
+        scientificName.text = ci.AbelisaurusScientificName;
+        popularName.text = ci.AbelisaurusNickname;
+
+        LoadPanel(Inventory.Abelisaurus);
+    }
+
+    public void GenerateDesmodus()
+    {
+        if(Inventory.Desmodus == UnlockSteps.Locked) return;
+
+        CatalogInfo ci = GetComponent<CatalogInfo>();
+
+        location.sprite = ci.DesmodusLocation;
+        description.text = ci.DesmodusText;
+        species.sprite = ci.DesmodusSprite;
+        speciesName.text = ci.DesmodusName;
+        scientificName.text = ci.DesmodusScientificName;
+        popularName.text = ci.DesmodusNickname;
+
+        LoadPanel(Inventory.Desmodus);
+    }
+
+    public void GenerateEremotherium()
+    {
+        if(Inventory.Eremotherium == UnlockSteps.Locked) return;
+
+        CatalogInfo ci = GetComponent<CatalogInfo>();
+
+        location.sprite = ci.EremotheriumLocation;
+        description.text = ci.EremotheriumText;
+        species.sprite = ci.EremotheriumSprite;
+        speciesName.text = ci.EremotheriumName;
+        scientificName.text = ci.EremotheriumScientificName;
+        popularName.text = ci.EremotheriumNickname;
+
+        LoadPanel(Inventory.Eremotherium);
+    }
+
+    public void GenerateGlyptodon()
+    {
+        if(Inventory.Glyptodon == UnlockSteps.Locked) return;
+
+        CatalogInfo ci = GetComponent<CatalogInfo>();
+
+        location.sprite = ci.GlyptodonLocation;
+        description.text = ci.GlyptodonText;
+        species.sprite = ci.GlyptodonSprite;
+        speciesName.text = ci.GlyptodonName;
+        scientificName.text = ci.GlyptodonScientificName;
+        popularName.text = ci.GlyptodonNickname;
+
+        LoadPanel(Inventory.Glyptodon);
+    }
+
+    public void GenerateVinctifer()
+    {
+        if(Inventory.Vinctifer == UnlockSteps.Locked) return;
+
+        CatalogInfo ci = GetComponent<CatalogInfo>();
+
+        location.sprite = ci.VinctiferLocation;
+        description.text = ci.VinctiferText;
+        species.sprite = ci.VinctiferSprite;
+        speciesName.text = ci.VinctiferName;
+        scientificName.text = ci.VinctiferScientificName;
+        popularName.text = ci.VinctiferNickname;
+
+        LoadPanel(Inventory.Vinctifer);
+    }
+
+    void LoadPanel(UnlockSteps step)
+    {
+        panel.SetActive(true);
+        unlockButton.SetActive(false);
+        buyButton.SetActive(false);
+        selectButton.SetActive(false);
+
+        if(step == UnlockSteps.FirstFossil)
+        {
+            unlockButton.SetActive(true);
+            unlockButton.transform.GetChild(0).GetComponent<Text>().text = "Colete mais fósseis desta espécie para desbloqueá-la\n\nProgresso: 33%";
+            location.sprite = question;
+            description.text = noDescription;
+        }
+        else if(step == UnlockSteps.SecondFossil)
+        {
+            unlockButton.SetActive(true);
+            unlockButton.transform.GetChild(0).GetComponent<Text>().text = "Colete mais fósseis desta espécie para desbloqueá-la\n\nProgresso: 66%";
+            description.text = noDescription;
+        }
+        else if(step == UnlockSteps.ThirdFossil)
+        {
+            buyButton.SetActive(true);
+        }
+        else if(step == UnlockSteps.Unlocked)
+        {
+            selectButton.SetActive(true);
+        }
     }
 }
