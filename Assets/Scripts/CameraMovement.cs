c49e38536569e1e208fbfc4e6a1bbbd065eb0757
lluckymou <lluckymou@gmail.com>
2022-07-18 17:30:46 -0300

    Better camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -11,17 +11,17 @@ public class CameraMovement : MonoBehaviour
         if(Health.Value <= 0) return;
 
         float position =  Input.mousePosition.x;
-        float objPosition = Camera.main.WorldToScreenPoint(transform.position).x;
+        float objPosition = Camera.main.WorldToScreenPoint(Player.instance.transform.position).x;
         
         if(position - objPosition >= 0) // right
         {
-            if(transform.position.x < 4.25f)
-                transform.Translate(transaltionRate * Time.deltaTime, 0, 0);
+            if(Player.instance.transform.position.x < 4.25f)
+                Player.instance.transform.Translate(transaltionRate * Time.deltaTime, 0, 0);
         }
         else 
         {
-            if(transform.position.x > -4.25f)
-                transform.Translate(-transaltionRate * Time.deltaTime, 0, 0);
+            if(Player.instance.transform.position.x > -4.25f)
+                Player.instance.transform.Translate(-transaltionRate * Time.deltaTime, 0, 0);
         }
     }
 }
